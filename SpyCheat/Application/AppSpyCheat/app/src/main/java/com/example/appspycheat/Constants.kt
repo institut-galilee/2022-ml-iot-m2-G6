74d51688157c8ecc847c57a0a06070e0c759de1f
Hajar-B <hajar.bouziane@outlook.com>
2022-02-24 15:04:19 +0100

    Application

---
+++ SpyCheat/Application/AppSpyCheat/app/src/main/java/com/example/appspycheat/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.appspycheat
+
+import android.Manifest
+
+/*
+* Source :
+* Aide caméraX (Vidéo Youtube) -> https://www.youtube.com/watch?v=HjXJh_vHXFs
+ */
+object Constants {
+    const val TAG = "cameraX"
+    const val REQUEST_CODE_PERMISSION = 123
+    val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+}
